25660c0057c15672c071b278480a01959b594399
arc680 <arc680@live.com>
2017-04-03 23:02:08 +0900

    SafariやAtomのプレビューで崩れていたのを修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,38 @@
-(function (window, document) {
-var menu = document.getElementById('menu'),
-    menu_list = document.getElementById('menu-list'),
-    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+(function(window, document) {
+    var menu = document.getElementsByTagName('header')[0],
+        WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange' : 'resize';
 
-function toggleHorizontal() {
-    [].forEach.call(
-        document.getElementById('menu').querySelectorAll('.custom-can-transform'),
-        function(el){
-            el.classList.toggle('pure-menu-horizontal');
+    function toggleHorizontal() {
+        [].forEach.call(
+            document.getElementsByTagName('header')[0].querySelectorAll('.custom-can-transform'),
+            function(el) {
+                el.classList.toggle('pure-menu-horizontal');
+            }
+        );
+    };
+
+    function toggleMenu() {
+        // set timeout so that the panel has a chance to roll up
+        // before the menu switches states
+        if (menu.classList.contains('open')) {
+            setTimeout(toggleHorizontal, 500);
+        } else {
+            toggleHorizontal();
         }
-    );
-};
+        menu.classList.toggle('open');
+        document.getElementById('toggle').classList.toggle('x');
+    };
 
-function toggleMenu() {
-    // set timeout so that the panel has a chance to roll up
-    // before the menu switches states
-    if (menu.classList.contains('open')) {
-        setTimeout(toggleHorizontal, 500);
+    function closeMenu() {
+        if (menu.classList.contains('open')) {
+            toggleMenu();
+        }
     }
-    else {
-        toggleHorizontal();
-    }
-    menu.classList.toggle('open');
-    document.getElementById('toggle').classList.toggle('x');
-};
 
-function closeMenu() {
-    if (menu.classList.contains('open')) {
+    document.getElementById('toggle').addEventListener('click', function(e) {
         toggleMenu();
-    }
-}
+        e.preventDefault();
+    });
 
-document.getElementById('toggle').addEventListener('click', function (e) {
-    toggleMenu();
-    e.preventDefault();
-});
-
-window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+    window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
 })(this, this.document);
